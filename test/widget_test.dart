946d4702f63d6ccc14da29bd9a579baf39927c90
Tran Anh <ttanh.bk@gmail.com>
2019-10-16 16:08:16 +0900

    change test file for travis

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:firstflutter/main.dart';
 
 void main() {
-  testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+  testWidgets('Checking if hello world shows up - show or not', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
     expect(find.text('Hello World!'), findsOneWidget);
